5795d2fb5ebff35651f8fa910f905c5c47b2ba02
nethan <nethan.win@protonmail.com>
2024-08-06 14:50:07 +0300

    add hypr and waybar config

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo pacman -S hyprland waybar hyprpaper hyprlock wofi
+
+cp ./waybar ~/.config/
+cp ./hypr/ ~/.config/
+
+mkdir ~/pictures/wallpapers/
+cp ./wallpapers/laputa.jpg ~/pictures/wallpapers/
+
+#install jetbrains mono nerd font
+
+sudo pacman -S qutebrowser grim pactl slurp dolphin syncthing
+mkdir ~/pictures/Screenshots
