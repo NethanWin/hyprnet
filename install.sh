850f60128a82c87fac87fc6c0d693ecc8e19f0ec
nethan <nethan.win@protonmail.com>
2024-08-17 17:47:53 +0300

    fix mkdir

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-sudo pacman -S hyprland waybar hyprpaper hyprlock wofi ttf-jetbrains-mono-nerd -y
+sudo pacman -S --noconfirm --needed hyprland waybar hyprpaper hyprlock wofi ttf-jetbrains-mono-nerd -y
 
 cp -r ./waybar ~/.config/
 cp -r ./hypr/ ~/.config/
 
-mkdir ~/pictures/wallpapers
+mkdir -p ~/pictures/wallpapers
 cp ./wallpapers/laputa.jpg ~/pictures/wallpapers/
 
 #install jetbrains mono nerd font
 
-sudo pacman -S qutebrowser grim pactl slurp dolphin syncthing -y
-mkdir ~/pictures/Screenshots
+sudo pacman -S --noconfirm --needed konsole qutebrowser grim pactl slurp dolphin syncthing -y
+mkdir -p ~/pictures/Screenshots
